28effd268fd2d6793ec7ea4b67280d88cbd04207
unknown <olarewanju123@gmail.com>
2024-05-06 11:49:38 +0100

    my first deploy

---
+++ delay.js
@@ -0,0 +1,28 @@
+// function delay() {
+//     setTimeout(function() {
+//         window.location.href = 'home.html'
+//     }, 50000);
+// }
+// window.onload = delay
+
+
+var texts = ["WELCOME", "TO", "THE", "FUTURE"];
+
+
+function showTextWithDelay() {
+    var delay = 0;
+    for (var i = 0; i < texts.length; i++) {
+        setTimeout(function(text) {
+            return function() {
+                var divId = "div" + (texts.indexOf(text) + 1); // Get the div id
+                var textDiv = document.getElementById(divId); // Get the div element
+                textDiv.textContent = text; 
+            };
+        }(texts[i]), delay * 200); 
+
+        delay += 5;
+    }
+}
+
+
+showTextWithDelay();
